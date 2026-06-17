77d9702e9f058fc46eec12ddbb7d72f8bcc8ab3d
Josh Holmer <jholmer.in@gmail.com>
2026-06-17 13:06:05 -0400

    remove fmt customizations

--- examples/alloc_zero.rs
+++ examples/alloc_zero.rs
@@ -1,19 +1,23 @@
-use diol::Result;
 use diol::prelude::*;
+use diol::Result;
 
 fn alloc_zero(bencher: Bencher, PlotArg(n): PlotArg) {
-	bencher.bench(|| aligned_vec::avec_rt![[64] | 0f64; n]);
+    bencher.bench(|| aligned_vec::avec_rt![[64] | 0f64; n]);
 }
 
 fn alloc_nonzero(bencher: Bencher, PlotArg(n): PlotArg) {
-	bencher.bench(|| aligned_vec::avec_rt![[64] | 1f64; n]);
+    bencher.bench(|| aligned_vec::avec_rt![[64] | 1f64; n]);
 }
 
 fn main() -> Result<()> {
-	let bench = Bench::from_args()?;
+    let bench = Bench::from_args()?;
 
-	bench.register_many("alloc", list![alloc_zero, alloc_nonzero], (4..25).map(|i| PlotArg(1 << i)));
-	bench.run()?;
+    bench.register_many(
+        "alloc",
+        list![alloc_zero, alloc_nonzero],
+        (4..25).map(|i| PlotArg(1 << i)),
+    );
+    bench.run()?;
 
-	Ok(())
+    Ok(())
 }
--- tests/compile_fail.rs
+++ tests/compile_fail.rs
@@ -1,5 +1,5 @@
 #[test]
 fn copy_from_ptr_requires_unsafe() {
-	let tests = trybuild::TestCases::new();
-	tests.compile_fail("tests/ui/copy_from_ptr_requires_unsafe.rs");
+    let tests = trybuild::TestCases::new();
+    tests.compile_fail("tests/ui/copy_from_ptr_requires_unsafe.rs");
 }
